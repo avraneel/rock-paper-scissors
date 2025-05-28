9fc40f30f63be449a4361243131b16b735d2468f
Avraneel Pal <avraneelpal22@gmail.com>
2025-05-28 20:07:19 +0545

    Add logic for single round match

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound() {
+    const human = getHumanChoice().toLowerCase();
+    const computer = getComputerChoice();
+    console.log("Computer plays " + computer);
+
+    if(human == "rock" && computer == "paper" || human == "paper" && computer == "scissors" || human == "scissors" && computer == "rock") {
+        computerScore++;
+        console.log("Computer wins.");
+    }
+    else if(human == "paper" && computer == "rock" || human == "scissors" && computer == "paper" || human == "rock" && computer == "scissors") {
+        humanScore++;
+        console.log("Human wins.");
+    }
+    else {
+        console.log("Draw round.")
+    }
+}
+
+playRound();
